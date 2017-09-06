4d6c4ea7baaf8d8ae3418cb2ab46cac6f7a8a3a4
smallfriesss <papiniu@outlook.com>
2017-09-06 10:55:12 -0700

    initial commit

---
+++ themes/the_theme/js/scripts.js
@@ -0,0 +1,21 @@
+jQuery(".icon-container").click(function () {
+    jQuery(".hamburger").toggleClass("expanded");
+    jQuery("body").toggleClass("menu-push");
+    jQuery("#menu").toggleClass("menu-open");
+    jQuery(".menu-text").toggleClass("expanded-text");
+  });
+  
+  
+  jQuery(document).ready(function(jQuery) {
+    jQuery('a[href^="#"]').bind('click.smoothscroll',function (e) {
+      e.preventDefault();
+      var target = this.hash,
+          jQuerytarget = jQuery(target);
+  
+      jQuery('html, body').stop().animate( {
+        'scrollTop': jQuerytarget.offset().top-40
+      }, 900, 'swing', function () {
+        window.location.hash = target;
+      } );
+    } );
+  } );
